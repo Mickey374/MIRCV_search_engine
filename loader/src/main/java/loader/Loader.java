420101d653e16f5393b73f31ef5cbf529fd425df
Mickey374 <asantemichael37483@gmail.com>
2024-02-10 01:28:24 +0100

    Loading the tsv files for initial test

---
+++ loader/src/main/java/loader/Loader.java
@@ -0,0 +1,14 @@
+package loader;
+
+import java.io.BufferedReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Loader {
+    private static final String DATA_PATH = "./data/collection.tsv";
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
