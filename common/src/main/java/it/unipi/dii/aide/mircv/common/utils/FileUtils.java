f4d4225cf5cd8f079e23e8e9727407ad395e3750
Mickey374 <asantemichael37483@gmail.com>
2024-04-24 20:32:35 +0200

    Refactored to include writing file to disk and checking if file exists

---
+++ common/src/main/java/it/unipi/dii/aide/mircv/common/utils/FileUtils.java
@@ -0,0 +1,29 @@
+package it.unipi.dii.aide.mircv.common.utils;
+
+
+import java.io.File;
+import java.io.PrintWriter;
+
+
+public class FileUtils {
+
+    public static void CreateOrCleanFile(String path){
+        File file = new File(path);
+
+        try {
+            if(file.createNewFile()){
+                System.out.println("File created: " + file.getName() + " at path: " + file.getPath());
+
+            } else {
+                System.out.println("File Already exists.");
+                try(PrintWriter writer = new PrintWriter(path)){
+                    writer.print("");
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
